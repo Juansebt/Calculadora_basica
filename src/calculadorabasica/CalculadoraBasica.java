27d7c564f6f8707ed8168496377a28fd4b49fa1f
Juanse <juansebt.0610@gmail.com>
2023-05-30 11:13:27 -0500

    files calculadora java

---
+++ src/calculadorabasica/CalculadoraBasica.java
@@ -0,0 +1,19 @@
+
+package calculadorabasica;
+
+/**
+ *
+ * @author SENA
+ */
+public class CalculadoraBasica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmCalculadora frm = new FrmCalculadora();
+        frm.setVisible(true);
+    }
+    
+}
